a7598e6da28ae973882452d607b55b31af372044
MALARVIZHI M <Malarm2004@gmail.com>
2024-01-23 09:27:21 +0530

    Booking website

---
+++ client/src/components/navbar/Navbar.jsx
@@ -0,0 +1,47 @@
+import "./navbar.css";
+import { Link, useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
+
+const Navbar = () => {
+  const { user } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const handleLoginClick = () => {
+    navigate("/login");
+  };
+
+  const handleRegisterClick = () => {
+    navigate("/register");
+  };
+
+
+  return (
+    <div className="navbar">
+      <div className="navContainer">
+        <Link to="/" style={{ color: "inherit", textDecoration: "none" }}>
+          <span className="logo">UVMbooking</span>
+        </Link>
+        {user ? (
+          <div className="navItems">
+          <span>{user.username}</span>
+            <button className="navButton">
+              Logout
+            </button>
+          </div>
+        ) : (
+          <div className="navItems">
+            <button className="navButton" onClick={handleRegisterClick}>
+              Register
+            </button>
+            <button className="navButton" onClick={handleLoginClick}>
+              Login
+            </button>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
